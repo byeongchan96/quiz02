3870c52f840b8da41132a1f300b86ae732ef832d
byeongchan96 <byeongchan0805@daum.net>
2024-10-18 00:43:05 +0900

    문제2번

---
+++ src/main/java/fullstack405/quiz02/Quiz02Application.java
@@ -0,0 +1,13 @@
+package fullstack405.quiz02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiz02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quiz02Application.class, args);
+	}
+
+}
---
+++ src/test/java/fullstack405/quiz02/Quiz02ApplicationTests.java
@@ -0,0 +1,13 @@
+package fullstack405.quiz02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quiz02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
